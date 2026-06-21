d34a7f3ec565c51d536d1369dc921ef3d2965ca1
Ashok S.R <ashok.106sr@gmail.com>
2026-06-21 12:33:54 +0530

    add the -l comman too

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include <sys/stat.h>
 #include <pwd.h>
 #include <grp.h>
+#include <time.h>
+
 //the function for the information abt the the specific file 
 void mode_string(mode_t mode, char *str){
     if (S_ISDIR(mode)) str[0] = 'd';
@@ -47,25 +49,33 @@ void print_long(const char *dir, const char *name){
   const char *user = pw ? pw->pw_name : "?";
   const char *group = pw ? pw->pw_name : "?";
 
-  printf("%s %s %s %s\n", modes, user, group, name);
+  char timebuf[64];
+  struct tm *tm = localtime(&st.st_mtim.tv_sec);
+  strftime(timebuf, sizeof(timebuf), "%b %e %H:%M", tm);
+
+  printf("%s %lu %s %s %ld %s %s \n", modes,(unsigned long)st.st_nlink ,user, group, (long)st.st_size, timebuf,name);
 }
 
 
 int show_all = 0;
+int long_formant = 0;
 
+//the main function
 int main (int argc, char *argv[]){
     int opt;
 //the "-a" command 
-    while ((opt = getopt(argc, argv, "a")) != -1)
+    while ((opt = getopt(argc, argv, "al")) != -1)
     {
         switch (opt)
         {
         case 'a' :
             show_all = 1;
             break;
-        
+        case 'l' :
+            long_formant = 1;
+            break;
         default:
-            fprintf(stderr, "usage: %s [-a] [path] \n", argv[0]);
+            fprintf(stderr, "usage: %s [-al] [path] \n", argv[0]);
             return 1;
         }
     }
@@ -82,8 +92,12 @@ int main (int argc, char *argv[]){
     while ((entry = readdir(dir)) != NULL)
     {
         if(!show_all && (*entry).d_name[0] == '.') continue;
-        print_long(path, entry->d_name);
-    }
+        if (long_formant) {
+          print_long(path, entry->d_name);
+        } else {
+          printf("%s\n", entry->d_name);
+        }
+      }
 
     closedir(dir);
     
